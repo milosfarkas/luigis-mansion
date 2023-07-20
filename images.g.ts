40e85e14dcf77cf284e8ce0a5b509dc02945221c
milosfarkas <128515846+milosfarkas@users.noreply.github.com>
2023-07-20 18:36:51 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Luigi":return img`
+. . . . . . . . 5 5 5 5 5 1 5 . 
+. . . . . . . . . 5 5 5 1 5 . . 
+. . . . . . . . . . f f f . . . 
+. . . . . . . . . . f f f . . . 
+. . . . . . . . d . . f . . . . 
+. . . . . . . 7 7 7 f f . . . . 
+. . . . . . 7 1 1 1 7 f . . . . 
+. . . . . 7 7 7 7 7 7 7 . . . . 
+. . . . . 7 7 7 7 7 7 7 f . . . 
+. . . . . 7 7 7 7 7 7 7 f . . . 
+. . . . . . 7 7 7 7 7 . f . . . 
+. . . . . . 2 2 f 2 2 f . . . . 
+. . . . . . . 2 2 f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
